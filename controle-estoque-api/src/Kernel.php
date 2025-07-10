cfc196f75a3c79ef344c09aa6e46ce0059109bee
Symon <symonlima07@gmail.com>
2025-07-10 15:33:28 -0300

    Alterações de configuração

--- controle-estoque-api/src/Kernel.php
+++ controle-estoque-api/src/Kernel.php
@@ -8,4 +8,13 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        // Define o fuso horário padrão para a aplicação
+        date_default_timezone_set('America/Sao_Paulo');
+    }
+
 }
